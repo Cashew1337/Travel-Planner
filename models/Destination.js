ccb8939b6dd6d782833be97a4e480f69344ded5d
JTNCLARKE <jtnclarke03@gmail.com>
2023-05-01 23:45:39 -0400

    added the models for the different tables, lmk if i needa change anything.

---
+++ models/Destination.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Destination extends Model {}
+
+Destination.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        country: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull : false
+        }
+    }
+)
+
+module.exports = Destination
