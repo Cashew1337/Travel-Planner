50e59f27e0b2af407a99b6d332fe2f107b924cff
Isaac Stofko <isaacstofko@gmail.com>
2023-05-01 21:34:39 -0400

    finished trip,user,destination seeds mostly started itinerary seeds, (might want to use 3rd party api for itinerary...?)

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,15 +1,17 @@
 const sequelize = require('../config/connection');
-const { User, Destination, Trip } = require('../models');
+const { User, Destination, Trip, Itinerary } = require('../models');
 
-const userData = require('./userData.json');
+const UserData = require('./userData.json');
 const DestinationData = require('./destinationData.json');
 const TripData = require('./tipData.json');
+const ItineraryData = require('./itineraryData.json')
 
 const seedDatabase = async () => {
     try {
-      await User.bulkCreate(usersData); // insert the users data into the database
-      await Trip.bulkCreate(tripsData); // insert the trips data into the database
-      await Destination.bulkCreate(destinationsData); //insert destinations into data base 
+      await User.bulkCreate(UserData); // insert the users data into the database
+      await Trip.bulkCreate(TripData); // insert the trips data into the database
+      await Destination.bulkCreate(DestinationData); //insert destinations into data base 
+      await Itinerary.bulkCreate(ItineraryData);
   
       console.log('Database seeding complete');
     } catch (error) {
