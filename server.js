f7f0da4a5bb3d950a572d82bdfebe880459a91ca
Roxy Osorio <roxy.osorio@moo.com>
2023-05-02 20:37:58 -0400

    server.js file

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers');
+// MAC check if this is correct ^^^^ for your route path
+const exphbs = require('express-handlebars');
+// if we are using helpers, we can insert here
+
+// again need to check if the path route is correct
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// if we are using helpers, we can insert helpers inside curly brackets
+const hbs = exphbs.create({});
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
+
+app.use(session(sess));
+
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(routes);
+
+// firing it into local host
+// will recheck code for sequelize.sync({ force: false })
+app.listen(port, () => {
+    console.log(`Code Name's Website is Working! ${port}`)
+  });
