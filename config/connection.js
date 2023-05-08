0949a6e9f44ec5a6e9ae111900473c68ce427d42
Mr. Macro <mr.matt.caschetto@gmail.com>
2023-05-08 19:02:15 -0400

    update connection for heroku

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
         process.env.DB_USER,
         process.env.DB_PASSWORD,
         {
-            host: 'localhost',
+            host: 'https://thawing-eyrie-64621.herokuapp.com/',
             dialect: 'mysql',
             port: 3306
         }
