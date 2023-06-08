bda13de824d412be25648210fbcf80acbc9cbae9
Mr. Macro <mr.matt.caschetto@gmail.com>
2023-06-08 18:16:23 -0400

    Fixed profile upload.

--- server.js
+++ server.js
@@ -19,24 +19,24 @@ const hbs = exphbs.create({ helpers });
 
 
 const app = express();
-const upload = multer({ dest: 'uploads/' }); 
+const upload = multer({ dest: 'uploads/' });
 const PORT = process.env.PORT || 3001;
 
 
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24 * 7,
-      httpOnly: true,
-      secure: false,
-      sameSite: "strict"
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict"
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 
 app.use(session(sess));
@@ -52,24 +52,32 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
+app.use(function (req, res, next) {
+  res.setHeader(
+    'Content-Security-Policy-Report-Only',
+    "script-src 'self' https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js;"
+  );
+  next();
+});
 app.use(routes);
 
 //endpoint for profil pic 
 app.post('/upload', isAuthenticated, upload.single('profilePicture'), (req, res) => {
+  console.log('test 2');
   res.send('Profile picture uploaded successfully');
 });
 // Middleware to check if the user is authenticated
 function isAuthenticated(req, res, next) {
-  if (req.session && req.session.userId) {
+  console.log(req.session);
+  if (req.session && req.session.user_id) {
     next();
   } else {
     res.status(401).send('Unauthorized');
   }
 }
 
-sequelize.sync({ force: false }).then(() => { app.listen(PORT, () => 
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () =>
     console.log(`Code Name's Website is Working! ${PORT}`));
-  });
+});
 
-  
