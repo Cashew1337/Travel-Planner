1e03d42c60dcfa37cf35da9d9a566b5697d77773
Isaac Stofko <isaacstofko@gmail.com>
2023-06-08 21:56:41 -0400

    hard coded homepage image

--- controllers/api/profileRoutes.js
+++ controllers/api/profileRoutes.js
@@ -8,45 +8,7 @@ const Profile = require('../../models/Profile');
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/upload' && req.method.toLowerCase() === 'post') {
-    const form = new formidable.IncomingForm();
 
-    form.uploadDir = 'uploads/';
-
-    // Parse the form data
-    form.parse(req, (err, fields, files) => {
-      if (err) {
-        console.error(err);
-        res.statusCode = 500;
-        res.end('Internal Server Error');
-        return;
-      }
-     
-      const file = files.profilePicture;
-
-      // Move uploaded file to a permanent location
-      const newPath = form.uploadDir + file.name;
-      fs.rename(file.path, newPath, (err) => {
-        if (err) {
-         
-          console.error(err);
-          res.statusCode = 500;
-          res.end('Internal Server Error');
-          return;
-        }
-
-        // File upload success
-        res.statusCode = 200;
-        res.end('Profile picture uploaded successfully.');
-      });
-    });
-  } else {
-    
-    res.statusCode = 404;
-    res.end('Not Found');
-  }
-});
 
 app.post('/upload', (req, res) => {
     const form = new formidable.IncomingForm();
